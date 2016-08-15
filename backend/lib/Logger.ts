d72f91d3efa668489f0fbfe160c37880b30eca41
cody <cevek@ya.ru>
2016-08-15 19:34:27 +0500

    Add Logger

---
+++ backend/lib/Logger.ts
@@ -0,0 +1,11 @@
+export class Logger {
+    log(...args:any[]) {
+        const dt = new Date();
+        const hours = ('0' + dt.getHours()).substr(-2);
+        const minutes = ('0' + dt.getMinutes()).substr(-2);
+        const seconds = ('0' + dt.getSeconds()).substr(-2);
+        const ms = ('00' + dt.getMilliseconds()).substr(-3);
+
+        console.log(`${hours}:${minutes}:${seconds}.${ms}`, ...args);
+    }
+}
