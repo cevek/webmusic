6212063e809018c412c3cc83b4b8e02d7796b056
cody <cevek@ya.ru>
2016-08-17 15:12:27 +0500

    Add name arg to logger

--- backend/lib/Logger.ts
+++ backend/lib/Logger.ts
@@ -1,4 +1,7 @@
 export class Logger {
+    constructor(public name = ''){
+
+    }
     log(...args:any[]) {
         const dt = new Date();
         const hours = ('0' + dt.getHours()).substr(-2);
@@ -6,6 +9,6 @@ export class Logger {
         const seconds = ('0' + dt.getSeconds()).substr(-2);
         const ms = ('00' + dt.getMilliseconds()).substr(-3);
 
-        console.log(`${hours}:${minutes}:${seconds}.${ms}`, ...args);
+        console.log(`${hours}:${minutes}:${seconds}.${ms} ${this.name}`, ...args);
     }
 }
