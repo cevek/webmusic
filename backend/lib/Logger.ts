e754d3652b782b79750dfee856e11c1b6f20e4fd
cody <cevek@ya.ru>
2016-08-18 14:14:21 +0500

    Add Logger error

--- backend/lib/Logger.ts
+++ backend/lib/Logger.ts
@@ -1,14 +1,28 @@
 export class Logger {
-    constructor(public name = 'main'){
+    constructor(public name = 'main') {
 
     }
-    log(...args:any[]) {
+
+    private _log(method: Function, args: any[]){
         const dt = new Date();
         const hours = ('0' + dt.getHours()).substr(-2);
         const minutes = ('0' + dt.getMinutes()).substr(-2);
         const seconds = ('0' + dt.getSeconds()).substr(-2);
         const ms = ('00' + dt.getMilliseconds()).substr(-3);
+        for (let i = 0; i < args.length; i++) {
+            const arg = args[i];
+            if (arg instanceof Error) {
+                args[i] = arg.stack;
+            }
+        }
+        method(`${hours}:${minutes}:${seconds}.${ms} ${this.name}`, ...args);
+    }
 
-        console.log(`${hours}:${minutes}:${seconds}.${ms} ${this.name}`, ...args);
+    log(...args:any[]) {
+        this._log(console.log, args);
+    }
+
+    error(...args:any[]) {
+        this._log(console.error, args);
     }
 }
